3bf4cb9b43b464aafd35084e53f8c6024a82e53e
antjimar <antonio.jimenez2@gmail.com>
2022-05-08 18:10:57 +0100

    wip

--- src/App.js
+++ src/App.js
@@ -1,18 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import firebase from "./utils/Firebase";
 import "firebase/compat/auth";
+import Auth from "./pages/Auth";
 
 function App() {
+  const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+
   firebase.auth().onAuthStateChanged(currentUser => {
-    console.log(currentUser ? "Estamos Logeados" : "No estamos Logeados");
+    if(!currentUser) {
+      setUser(null);
+    } else {
+      setUser(currentUser);
+    }
+    setIsLoading(false);
   });
 
-  console.log("Hola mundo")
-  return (
-    <div className="App">
-      <h1>App Electron + React</h1>
-    </div>
-  );
+  if(isLoading) {
+    return null;
+  }
+  return !user ? <Auth /> : <UserLogged />;
 }
+
+
+function UserLogged() {
+  const logout = () => {
+    firebase.auth().signOut();
+  }
+
+
+  return (
+    <div style={{ 
+      display: "flex", 
+      alignItems: "center", 
+      justifyContent: "center",
+      flexDirection: "column",
+      height: "100vh"
+    }}
+    >
+      <h1>Usuario Loggeado</h1>
+      <button onClick={logout}>Cerrar sesión</button>
+    </div>
+  )
+}
+
 
 export default App;
