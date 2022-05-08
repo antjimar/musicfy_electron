dc0fb335983d63fc5413d7ff0f6bc334a9bd8fab
antjimar <antonio.jimenez2@gmail.com>
2022-05-08 17:16:22 +0100

    Add semantic ui library

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import 'semantic-ui-css/semantic.min.css';
+import './index.scss';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
